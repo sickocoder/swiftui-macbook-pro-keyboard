ba4583cd52beb77ca3628975b20268ad5e16546d
Jose Tone <developer.tony17@gmail.com>
2023-01-12 10:27:33 +0100

    Initial Commit

---
+++ Macbook M1 Pro Keyboard/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Macbook M1 Pro Keyboard
+//
+//  Created by José Tony on 12/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Macbook M1 Pro Keyboard/Macbook_M1_Pro_KeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Macbook_M1_Pro_KeyboardApp.swift
+//  Macbook M1 Pro Keyboard
+//
+//  Created by José Tony on 12/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Macbook_M1_Pro_KeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
